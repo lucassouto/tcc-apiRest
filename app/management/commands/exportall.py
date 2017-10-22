720f1939dfd1143ea78a044a9733707da9c06e91
Lucas <lucasinfologos@gmail.com>
2017-10-22 03:37:27 -0300

    Added command exportall

---
+++ app/management/commands/exportall.py
@@ -0,0 +1,36 @@
+import os.path
+from django.core.management.base import BaseCommand
+from django.utils.six import BytesIO
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+from apiRest.utils.filescsv import create_many_register_csv, insert_many_register_csv
+from app.models import Book
+from app.serializers import BookSerializer
+
+
+class Command(BaseCommand):
+    help = 'Exports all records from the database'
+
+    def __init__(self):
+        super().__init__()
+        self.book = ''
+
+
+    def handle(self, *args, **options):
+        self.book = Book.objects.all()
+
+        serializer = BookSerializer(self.book, many=True)
+
+        json = JSONRenderer().render(serializer.data)
+        stream = BytesIO(json)
+        data = JSONParser().parse(stream)
+
+        columns_names = data[0].keys()
+
+        if os.path.isfile('register.csv'):
+            insert_many_register_csv('register.csv', data, columns_names)
+
+        else:
+            create_many_register_csv('register.csv', data, columns_names)
+
